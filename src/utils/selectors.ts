a197861d23caac06acc71975ed319dc322d5581f
Suman Sarkar <suman@codebuddy.co>
2022-09-09 17:43:11 +0530

    add support for findMany, update, updateMany, upsert, delete, deleteMany

--- src/utils/selectors.ts
+++ src/utils/selectors.ts
@@ -1,4 +1,10 @@
 export const ANY_FIND_SELECTOR =
    'Identifier:matches(' +
    '[name="findFirst"], ' +
+   '[name="findMany"], ' +
+   '[name="update"], ' +
+   '[name="updateMany"], ' +
+   '[name="upsert"], ' +
+   '[name="delete"], ' +
+   '[name="deleteMany"], ' +
    '[name="findFirstOrThrow"])'
