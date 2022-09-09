f86956d5c955e99c231d43f1c0f21b13e26fcca1
Suman Sarkar <suman@codebuddy.co>
2022-09-09 17:59:24 +0530

    remove support for update, upsert, delete

--- src/utils/selectors.ts
+++ src/utils/selectors.ts
@@ -2,9 +2,6 @@ export const ANY_FIND_SELECTOR =
    'Identifier:matches(' +
    '[name="findFirst"], ' +
    '[name="findMany"], ' +
-   '[name="update"], ' +
    '[name="updateMany"], ' +
-   '[name="upsert"], ' +
-   '[name="delete"], ' +
    '[name="deleteMany"], ' +
    '[name="findFirstOrThrow"])'
